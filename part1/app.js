18974afc64e72f2c164be90323f4a4150b09d388
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 02:11:47 +0000

    [automatic commit] 2025-06-20 02:11:47 UTC 121.44.208.219

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
+
 var app = express();
 var db = require('./db'); //////////////////////////
 
@@ -17,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // //debug connection
 // (async () => {
