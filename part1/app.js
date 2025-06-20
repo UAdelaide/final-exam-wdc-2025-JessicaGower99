e81b0cb8811e3ba31b7b11f76ac6ad15ba84435e
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 02:15:09 +0000

    [automatic commit] 2025-06-20 02:15:09 UTC 121.44.208.219

--- part1/app.js
+++ part1/app.js
@@ -68,22 +68,4 @@ app.use('/dogs', dogsRouter); /////////////////////
 // })();
 
 
-// /////////////////
-// //get dogs as json
-// app.get('/api/dogs', async (req, res) => {
-//     try {
-//         const [rows] = await db.query(`
-//       SELECT dog.name AS dog_name, dog.size, user.username AS owner_username
-//       FROM Dogs dog
-//       JOIN Users user ON dog.owner_id = user.user_id
-//     `);
-//         res.json(rows);
-//     } catch (err) {
-//         console.error("DATABASE CONNECTION ERROR:", err); // try to debug db connection issue
-//         res.status(500).json({ error: 'Failed to retrieve dogs.' });
-//     }
-// });
-// ///////////////
-
-
 module.exports = app;
--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,4 +3,19 @@ var router = express.Router();
 var db = require('../db');
 
 
+//get dogs as json
+router.get('/api/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.query(`
+      SELECT dog.name AS dog_name, dog.size, user.username AS owner_username
+      FROM Dogs dog
+      JOIN Users user ON dog.owner_id = user.user_id
+    `);
+        res.json(rows);
+    } catch (err) {
+        console.error("DATABASE CONNECTION ERROR:", err); // try to debug db connection issue
+        res.status(500).json({ error: 'Failed to retrieve dogs.' });
+    }
+});
+
 module.exports = router;
