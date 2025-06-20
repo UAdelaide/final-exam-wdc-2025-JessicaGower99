26e8e77632cdf7b2ccabe8d16e4e0a59c7592a39
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 13:36:24 +0000

    [automatic commit] 2025-06-20 13:36:24 UTC 121.44.208.219

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/owners/:ownerId/dogs', async (req, res) => {
-    const ownerId = req.params.ownerId;
+    const ownerId = req.session.userId;
     try {
         const [dogs] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]);
         res.json(dogs);
