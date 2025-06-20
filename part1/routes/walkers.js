f514729b53819c5706d1e01003721bbeaa1071b7
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 03:05:11 +0000

    [automatic commit] 2025-06-20 03:05:11 UTC 121.44.208.219

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -6,13 +6,17 @@ var db = require('../db');
 router.get('/summary', async (requestAnimationFrame, res) => {
     try {
         const [rows] = await db.query('
-            SELECT user.username AS walker_username, COUNT(rate.rating_id) AS total_ratings, ROUND(AVG(rate.rating), 1) AS average_rating, COUNT(CASE WHEN w.status = 'completed' THEN 1 END) AS completed_walks
-            FROM Users user
+                SELECT
+        user.username AS walker_username,
+            COUNT(rate.rating_id) AS total_ratings,
+            ROUND(AVG(rate.rating), 1) AS average_rating,
+            COUNT(CASE WHEN w.status = 'completed' THEN 1 END) AS completed_walks
+      FROM Users user
       LEFT JOIN Walks w ON user.user_id = w.walker_id
-      LEFT JOIN Ratings rate ON w.walk_id = rate.walk_id
+      LEFT JOIN Ratings rrate ON w.walk_id = rate.walk_id
       WHERE user.user_type = 'walker'
-
-GROUP BY user.user_id');
+      GROUP BY user.user_id
+');
             res.json(rows);
     } catch (err) {
         console.error("Dang, database error when retrieving walkers:", err); // try to debug db connection issue
