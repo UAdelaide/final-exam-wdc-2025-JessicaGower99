7c44e9c3efbd59e9ba4c9d96e37234246bacfd4e
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 01:35:48 +0000

    [automatic commit] 2025-06-20 01:35:48 UTC 121.44.208.219

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,9 @@
-const mysql = require(‘mysql2/promise’);
+const mysql = require('mysql2/promise');
+
 const db = mysql.createPool({
-	Host: ‘localhost’,
-	User: ‘root’,
+	Host: 'localhost,
+	User: ‘root',
 password: ‘mypassword’,
 database: ‘textbook_marketplace’});
-Module.exports =db;
+
+module.exports =db;
