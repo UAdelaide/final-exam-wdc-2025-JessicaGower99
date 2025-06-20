8b51ea7bf38719f642e716af82521dbb2cc201c4
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 03:07:11 +0000

    [automatic commit] 2025-06-20 03:07:11 UTC 121.44.208.219

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var db = require('../db');
 
 // get walkers as json - path /api/walkers/summary'
-router.get('/summary', async (requestAnimationFrame, res) => {
-    try {
-        const [rows] = await db.query(`
+router.get('/summary', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
       SELECT
         user.username AS walker_username,
         COUNT(rate.rating_id) AS total_ratings,
@@ -17,11 +17,11 @@ router.get('/summary', async (requestAnimationFrame, res) => {
       WHERE user.user_type = 'walker'
       GROUP BY user.user_id
     `);
-        res.json(rows);
-    } catch (err) {
-        console.error("Dang, database error when retrieving walkers:", err); // try to debug db connection issue
-        res.status(500).json({ error: 'Failed to get walkers (really big sad face).' });
-    }
+    res.json(rows);
+  } catch (err) {
+    console.error("Dang, database error when retrieving walkers:", err); // try to debug db connection issue
+    res.status(500).json({ error: 'Failed to get walkers (really big sad face).' });
+  }
 });
 
 module.exports = router;
