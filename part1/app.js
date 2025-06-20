662959aacd395c034b2fe5f086b4af6eee75fb2c
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 02:18:09 +0000

    [automatic commit] 2025-06-20 02:18:09 UTC 121.44.208.219

--- part1/app.js
+++ part1/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dogs', dogsRouter); /////////////////////
+app.use('/api/dogs', dogsRouter); /////////////////////
 
 module.exports = app;
 
