b289747eb7217280b216cf37e4ec334daff4a592
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 03:05:51 +0000

    [automatic commit] 2025-06-20 03:05:51 UTC 121.44.208.219

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -5,18 +5,19 @@ var db = require('../db');
 // get walkers as json - path /api/walkers/summary'
 router.get('/summary', async (requestAnimationFrame, res) => {
     try {
-        const [rows] = await db.query('    SELECT
-        user.username AS walker_username,
-            COUNT(rate.rating_id) AS total_ratings,
-            ROUND(AVG(rate.rating), 1) AS average_rating,
-            COUNT(CASE WHEN w.status = 'completed' THEN 1 END) AS completed_walks
-      FROM Users user
-      LEFT JOIN Walks w ON user.user_id = w.walker_id
-      LEFT JOIN Ratings rrate ON w.walk_id = rate.walk_id
-      WHERE user.user_type = 'walker'
-      GROUP BY user.user_id
-');
-            res.json(rows);
+        const [rows] = await db.query(`
+      SELECT
+        u.username AS walker_username,
+        COUNT(r.rating_id) AS total_ratings,
+        ROUND(AVG(r.rating), 1) AS average_rating,
+        COUNT(CASE WHEN w.status = 'completed' THEN 1 END) AS completed_walks
+      FROM Users u
+      LEFT JOIN Walks w ON u.user_id = w.walker_id
+      LEFT JOIN Ratings r ON w.walk_id = r.walk_id
+      WHERE u.user_type = 'walker'
+      GROUP BY u.user_id
+    `);
+        res.json(rows);
     } catch (err) {
         console.error("Dang, database error when retrieving walkers:", err); // try to debug db connection issue
         res.status(500).json({ error: 'Failed to get walkers (really big sad face).' });
