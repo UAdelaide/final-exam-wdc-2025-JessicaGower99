440663c6f390b8401cd05a0570e58359bac7891c
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 12:23:03 +0000

    [automatic commit] 2025-06-20 12:23:03 UTC 121.44.208.219

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,7 +68,8 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req,res)) => {
   req.session.destry(err => {
     if (err){
-      return res.status(500).json({ error: 'Log'})
+      return res.status(500).json({ error: 'Loggout failed.' })
+    
     }
   })
 }
