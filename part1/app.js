c904346f574716cbe0c1fc97ed45d086ae3af5ef
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 01:47:33 +0000

    [automatic commit] 2025-06-20 01:47:33 UTC 121.44.208.219

--- part1/app.js
+++ part1/app.js
@@ -31,6 +31,7 @@ app.get('/api/dogs', async (req, res) => {
     `);
         res.json(rows);
     } catch (err) {
+        console.error(err);
         res.status(500).json({ error: 'Failed to retrieve dogs.' });
     }
 });
