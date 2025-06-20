8b771f94f8aae6c50e0f67575a6a4d7df6e4f9ec
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 12:37:30 +0000

    [automatic commit] 2025-06-20 12:37:30 UTC 121.44.208.219

--- part2/app.js
+++ part2/app.js
@@ -18,7 +18,7 @@ app.use(session({
     secret: 'this_is_secret',
     resave: false,
     saveUninitialized: false, // so wasted session on those thta visit but dont loggin
-    cookie: { /////////////////////////////////////////////////////////////////////////////////
+    cookie: { // define the cookie
         secure: false, // true if using https (using http so false)
         httpOnly: true,
         maxAge: 1000 * 60 * 60 // the cookie lives for 1 hour
