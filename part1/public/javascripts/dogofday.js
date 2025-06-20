65b9aec60a0bba77c9c093e79b4c80bd55c11e4b
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 05:06:34 +0000

    [automatic commit] 2025-06-20 05:06:34 UTC 121.44.208.219

--- part1/public/javascripts/dogofday.js
+++ part1/public/javascripts/dogofday.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
             };
         },
         mounted() {
-            fetch('https://dog.ceo/api/breeds/image/random')
+            fetch('https://dog.ceo/api/breeds/image/random'){
                 .then(res => res.json())
                 .then(data => {
                     this.dogImageUrl = data.message;
