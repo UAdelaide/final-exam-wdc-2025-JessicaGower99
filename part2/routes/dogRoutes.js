99dc0b0e02253971d619cb5e9e68ef9f703730e0
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 13:43:47 +0000

    [automatic commit] 2025-06-20 13:43:47 UTC 121.44.208.219

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/owners/dogs', async (req, res) => {
- const ownerId = req.session.user?.id;
+    const ownerId = req.session.user?.id;
 
-  if (!ownerId) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
+    if (!ownerId) {
+        return res.status(401).json({ error: 'Not logged in' });
+    }
     try {
         const [dogs] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]);
         res.json(dogs);
