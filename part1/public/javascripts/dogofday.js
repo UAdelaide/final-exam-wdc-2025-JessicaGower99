a92233565acecd60a7b3c07d287c548e9e2e127c
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 05:03:13 +0000

    [automatic commit] 2025-06-20 05:03:13 UTC 121.44.208.219

--- part1/public/javascripts/dogofday.js
+++ part1/public/javascripts/dogofday.js
@@ -1,23 +1,27 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            dogImgURL: '',
-            dogName: 'Stinky',
-            dogBreed: 'Unknown',
-            dogAge: '3 years'
-        };
-    },
-    // get random image of dog
-    mounted() {
-        fetch('https://dog.ceo/api/breeds/image/random')
-            .then(res => res.json())
-            .then(data => {
-                this.dogImageUrl = data.message;
-            })
-            .catch(err => {
-                console.error('Failed to a cute dog image:', err);
-            });
-    }
-});
+console.log('Loaded: vue-setup.js');
 
-app.mount('#app');
+document.addEventListener('DOMContentLoaded', function () {
+
+    const app = createApp({
+        data() {
+            return {
+                dogImgURL: '',
+                dogName: 'Stinky',
+                dogBreed: 'Unknown',
+                dogAge: '3 years'
+            };
+        },
+        // get random image of dog
+        mounted() {
+            fetch('https://dog.ceo/api/breeds/image/random')
+                .then(res => res.json())
+                .then(data => {
+                    this.dogImageUrl = data.message;
+                })
+                .catch(err => {
+                    console.error('Failed to a cute dog image:', err);
+                });
+        }
+    });
+
+    app.mount('#app');
