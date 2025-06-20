a112e7bf76c99916ef50ea55e0f91927667ad87e
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 01:59:57 +0000

    [automatic commit] 2025-06-20 01:59:57 UTC 121.44.208.219

--- part1/app.js
+++ part1/app.js
@@ -19,22 +19,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/////////////////
-//get dogs as json
-app.get('/api/dogs', async (req, res) => {
-    try {
-        const [rows] = await db.query(`
-      SELECT dog.name AS dog_name, dog.size, user.username AS owner_username
-      FROM Dogs dog
-      JOIN Users user ON dog.owner_id = user.user_id
-    `);
-        res.json(rows);
-    } catch (err) {
-        console.error("DATABASE ERROR:", err); // try to debug db connection issue
-        res.status(500).json({ error: 'Failed to retrieve dogs.' });
-    }
-});
-///////////////
+// /////////////////
+// //get dogs as json
+// app.get('/api/dogs', async (req, res) => {
+//     try {
+//         const [rows] = await db.query(`
+//       SELECT dog.name AS dog_name, dog.size, user.username AS owner_username
+//       FROM Dogs dog
+//       JOIN Users user ON dog.owner_id = user.user_id
+//     `);
+//         res.json(rows);
+//     } catch (err) {
+//         console.error("DATABASE ERROR:", err); // try to debug db connection issue
+//         res.status(500).json({ error: 'Failed to retrieve dogs.' });
+//     }
+// });
+// ///////////////
 
 
 module.exports = app;
