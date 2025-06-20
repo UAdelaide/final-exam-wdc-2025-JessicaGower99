5d3e96aad365dabbbeb6e3ace7ee23d85ade4218
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 02:55:47 +0000

    [automatic commit] 2025-06-20 02:55:47 UTC 121.44.208.219

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -2,4 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-// get walkers as json
+// get walkers as json - path /api/walkers/summary'
+
+
+module.exports = router;
