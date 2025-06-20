2e628e410b58c39e6dd2596f38978564136dd3ef
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 02:43:23 +0000

    [automatic commit] 2025-06-20 02:43:23 UTC 121.44.208.219

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -5,7 +5,12 @@ var db = require('../db');
 // get open walk requests - path /api/walkrequests/open
 router.get('/open', async (req, res) => {
 
-    try { } catch (err) { }
+    try {   const [rows] = await db.query(`
+      SELECT wr.id, wr.date, wr.time, d.name AS dog_name
+      FROM WalkRequests wr
+      JOIN Dogs d ON wr.dog_id = d.dog_id
+      WHERE wr.status = 'open'
+    `);} catch (err) { }
 });
 
 
