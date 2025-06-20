5de56efa34ab3b7f660bf855fb2d4da93c464703
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-20 13:50:09 +0000

    [automatic commit] 2025-06-20 13:50:09 UTC 121.44.208.219

--- part2/app.js
+++ part2/app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dogRoutes'); /////////////////
+const dogRoutes = require('./routes/dogRoutes');
 
 // session managemnt
 app.use(session({
@@ -28,7 +28,6 @@ app.use(session({
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api', dogRoutes); /////////////////
-
+app.use('/api', dogRoutes);
 // Export the app instead of listening here
 module.exports = app;
