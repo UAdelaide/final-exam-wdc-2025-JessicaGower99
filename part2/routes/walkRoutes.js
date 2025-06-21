926afa905ab60b38e16541035e722769fa01efb0
JessicaGower99 <jessica.gower@student.adelaide.edu.au>
2025-06-21 02:44:28 +0000

    [automatic commit] 2025-06-21 02:44:28 UTC 121.44.208.219

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -2,22 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET all walk requests (for walkers to view)
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query(`
-      SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
-      FROM WalkRequests wr
-      JOIN Dogs d ON wr.dog_id = d.dog_id
-      JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'
-    `);
-    res.json(rows);
-  } catch (error) {
-    console.error('SQL Error:', error);
-    res.status(500).json({ error: 'Failed to fetch walk requests' });
-  }
-});
+// // GET all walk requests (for walkers to view)
+// router.get('/', async (req, res) => {
+//   try {
+//     const [rows] = await db.query(`
+//       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
+//       FROM WalkRequests wr
+//       JOIN Dogs d ON wr.dog_id = d.dog_id
+//       JOIN Users u ON d.owner_id = u.user_id
+//       WHERE wr.status = 'open'
+//     `);
+//     res.json(rows);
+//   } catch (error) {
+//     console.error('SQL Error:', error);
+//     res.status(500).json({ error: 'Failed to fetch walk requests' });
+//   }
+// });
 
 //POST a new walk request (from owner)
 router.post('/', async (req, res) => {
@@ -75,7 +75,7 @@ router.get('/my-requests', async (req, res) => {
   }
 
   try {
-  const [rows] = await db.query(`
+    const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
